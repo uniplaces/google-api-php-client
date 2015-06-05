cc3edd31c3580198c523ab7fc063b1427ba2c0c9
joanabotelho <joanafpb@gmail.com>
2015-06-05 11:40:12 +0100

    Fix in translations

--- src/Google/Service/Translate.php
+++ src/Google/Service/Translate.php
@@ -337,7 +337,7 @@ class Google_Service_Translate_TranslationsListResponse extends Google_Collectio
   }
   public function getTranslations()
   {
-    return $this->translations;
+    return $this->modelData['data']['translations'];
   }
 }
 
